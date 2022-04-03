1e209a330ffb2024df669dbeb3890e1654087f7a
RabbitSellingWatermelon <Vader.Wenbo.Zhao@gmail.com>
2022-04-02 20:46:43 -0500

    some changes

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express');
+const { stringify } = require('querystring');
 const MongoClient = require('mongodb').MongoClient; 
 // const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3001;
 const url = "mongodb://localhost:27017/comp539"; 
 
-
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    console.log("Database created");
-    var dbase = db.db("Comp590");
-
-    dbase.createCollection("newsPosts", function (err, res) {
-        if (err) throw err;
-        console.log("Collection newsPosts created");
-    });
-
-    dbase.createCollection("comments", function (err, res) {
-        if (err) throw err;
-        console.log("Collection comments created");
-        db.close();
-    });
+app.get('/post.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "post.html" );
 });
+
+app.get('/comment.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "comment.html" );
+});
+
+// MongoClient.connect(url, function (err, db) {
+//     if (err) throw err;
+//     console.log("Database created");
+//     var dbase = db.db("Comp590");
+
+//     dbase.createCollection("newsPosts", function (err, res) {
+//         if (err) throw err;
+//         console.log("Collection newsPosts created");
+//     });
+
+//     dbase.createCollection("comments", function (err, res) {
+//         if (err) throw err;
+//         console.log("Collection comments created");
+//         db.close();
+//     });
+// });
 
 
 app.get('/get_posts', function(req, res) {
@@ -29,83 +37,88 @@ app.get('/get_posts', function(req, res) {
         if (err) throw err;
         console.log("Connected to MongoDB"); 
 
-        var newsCollection = db.db("Comp590");
+        var comp590DB = db.db("Comp590");
         var whereStr = {
             "sateName" : req.query.stateName,
             "date" : req.query.date
-        }
+        };
 
-        newsCollection.collection("newsPosts").find(whereStr).toArray(function(err, result) {
+        comp590DB.collection("newsPosts").find(whereStr).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
             res.json(result[0]);
             db.close();
         });
     });
-})
+});
 
-app.post('/new_post', function(req, res) {
-    MongoDB.connect(url, function(err, db) {
+app.get('/new_post', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
         if (err) throw err;
 
-        var newsCollection = db.db("Comp590");
+        console.log(req.query.stateName);
+
+        var comp590DB = db.db("Comp590");
         var dataToAdd = {
             "stateName" : req.query.stateName,
             "date" : req.query.date,
             "content" : req.query.content,
             "publisher" : req.query.publisher,
             "newsID" : req.query.newsID
-        }
+        };
 
-        newsCollection.collection("newsPosts").insertOne(dataToAdd, function(err, res) {
+        console.log(stringify(dataToAdd));
+
+        comp590DB.collection("newsPosts").insertOne(dataToAdd, function(err, res) {
             if (err) throw err;
             console.log("Inserted news/post successfully");
-            // res.end("success");
             db.close();
         });
     });
-}) 
+
+    res.end("Success");
+});
 
 app.get('/get_comments', function(req, res) {
     MongoClient.connect(url, function(err, db) { 
         if (err) throw err;
         console.log("Connected to MongoDB"); 
 
-        var newsCollection = db.db("Comp590");
+        var comp590DB = db.db("Comp590");
         var whereStr = {
-            "newsID" : "123123123"
-        }
+            "newsID" : req.query.newsID
+        };
 
-        newsCollection.collection("comments").find(whereStr).toArray(function(err, result) {
+        comp590DB.collection("comments").find(whereStr).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
-            res.json(result[0]);
             db.close();
         });
     });
-})
+});
 
-app.post('/new_comment', function(req, res) {
-    MongoDB.connect(url, function(err, db) {
+app.get('/new_comment', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
         if (err) throw err;
 
-        var newsCollection = db.db("Comp590");
+        var comp590DB = db.db("Comp590");
         var dataToAdd = {
             "newsID" : req.query.newsID,
             "commentID" : req.query.commentID,
             "commenterName" : req.query.commenterName,
             "content" : req.query.content,
             "date" : req.query.date
-        }
+        };
 
-        newsCollection.collection("comments").insertOne(dataToAdd, function(err, res) {
+        comp590DB.collection("comments").insertOne(dataToAdd, function(err, res) {
             if (err) throw err;
             console.log("Inserted comment successfully");
-            // res.end("success");
             db.close();
         });
     });
-})
+
+    res.end("Success");
+});
 
 var server = app.listen(port, function () {
  
@@ -113,4 +126,4 @@ var server = app.listen(port, function () {
     var port = server.address().port
    
     console.log("Running on http://%s:%s", host, port)
-})
+});
