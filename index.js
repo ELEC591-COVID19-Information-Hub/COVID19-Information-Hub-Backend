a2a044773d3a00c42e4d66d33d76419f50254d53
Jerrypoi <jerry.poi.sz@gmail.com>
2022-04-28 23:24:57 -0500

    refactor auth

--- index.js
+++ index.js
@@ -1,249 +1,32 @@
-<<<<<<< HEAD
-const express = require('express')
+const express = require("express");
+const process = require("process");
 const auth = require('./src/auth');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const mongoose = require("mongoose");
+const cors = require("cors");
 
-// todo: update mongoose address
-const connectionString = 'mongodb+srv://new-user1:123@cluster0.rjizs.mongodb.net/backend?retryWrites=true&w=majority';
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+const app = express()
+// TODO: determine front-end address
+// app.use(cors({
+//     origin: '',
+//     credentials: true,
+// }))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
-
-=======
-const express = require('express');
-const { stringify } = require('querystring');
-const MongoClient = require('mongodb').MongoClient; 
->>>>>>> origin/wenbo
-const app = express();
-app.use(bodyParser.json());
-app.use(cookieParser());
 auth(app);
 
+app.get('/', (req, resp) => {
+    resp.send('test')
+})
 
-const port = process.env.PORT || 3001;
-const url = "mongodb://localhost:27017/comp539"; 
+const port = process.env.PORT || 3000;
 
-
-const url = "mongodb://localhost:27017"; 
-const MongoClient = require('mongodb').MongoClient; 
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.get('/texas', (req, res) => {
-
-    MongoClient.connect(url, function(err, db) { 
-        if (err) throw err; 
-        console.log("Connected to MongoDB!"); 
-        var covidData = db.db("Comp590").collection("Covid_Data");
-        var whereStr = {"state" : "Texas", "date" : "2020-02-18"};
-
-        covidData.find(whereStr).toArray(function(err,result) { 
-            if (err) throw err;
-            console.log(result);
-            res.json(result[0]);
-            db.close(); 
-        });
-
-    });
-
-});
-
-
-
-// Frontend send JSON data to backend
-app.use(express.json());
-
-// // Create the database and collections
-// MongoClient.connect(url, function (err, db) {
-//     if (err) throw err;
-//     console.log("Database created");
-//     var dbase = db.db("Comp590");
-
-//     dbase.createCollection("newsPosts", function (err, res) {
-//         if (err) throw err;
-//         console.log("Collection newsPosts created");
-//     });
-
-//     dbase.createCollection("comments", function (err, res) {
-//         if (err) throw err;
-//         console.log("Collection comments created");
-//         db.close();
-//     });
-// });
-
-// Current states news topic list. need: [stateName, date]
-app.get('/get_news_topics', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        console.log("Connected to MongoDB");
-
-        var comp590DB = db.db("Comp590");
-
-        // current state
-        var state = {
-            "stateName" : req.body.stateName
-        };
-
-        var options = {
-            // project only topic, ID, and date
-            projection : {
-                '_id': 1,
-                'topic' : 1,
-                'date' : 1
-            }
-        }
-
-        comp590DB.collection("newsPosts").find(state, options).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.json(result[0]);
-            db.close();
-        });
-    });
-});
-
-
-// Get the news data, classified by sateName and date, need: [stateName, date]
-app.get('/get_news_date', function(req, res) {
-    MongoClient.connect(url, function(err, db) { 
-        if (err) throw err;
-        console.log("Connected to MongoDB"); 
-
-        var comp590DB = db.db("Comp590");
-        var whereStr = {
-            "sateName" : req.body.stateName,
-            "date" : req.body.date
-        };
-
-        comp590DB.collection("newsPosts").find(whereStr).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.json(result[0]);
-            db.close();
-        });
-    });
-});
-
-
-// Submit new post, need: [stateName, topic, date, content, publisher]
-app.post('/new_news', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-
-        var comp590DB = db.db("Comp590");
-
-        var dataToAdd = {
-            "stateName" : req.body.stateName,
-            "topic" : req.body.topic,
-            "date" : req.body.date,
-            "content" : req.body.content,
-            "publisher" : req.body.publisher,
-            // "newsID" : req.body.newsID
-        };
-
-        // console.log(stringify(dataToAdd));
-
-        comp590DB.collection("newsPosts").insertOne(dataToAdd, function(err, result) {
-            if (err) throw err;
-            console.log("Inserted news/post successfully");
-            // return new news' _id
-            res.json(result["insertedId"]);
-            db.close();
-        });
-    });
-});
-
-
-// Current news' comment list, need: [newsID]
-app.get('/get_comments', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        console.log("Connected to MongoDB"); 
-
-        var comp590DB = db.db("Comp590");
-
-        // the current news' id
-        var newsIDToFind = {
-            // news' ID
-            "newsID" : req.body.newsID
-        };
-
-        comp590DB.collection("comments").find(newsIDToFind).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.json(result[0]);
-            db.close();
-        });
-    });
-});
-
-
-// Submit a new comment, need: [newsID, commenterName, content, date, isResponse, replyTo]
-// if this is a comment replying to another comment, the <isResponse> should be true, the <replyTo> is 
-// the commentID of the comment that is replied.
-// Or the <isResponse> should be false and <replyTo> is ignored.
-app.put('/new_comment', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        console.log("Connected to MongoDB"); 
-
-        var comp590DB = db.db("Comp590");
-        var dataToAdd = {
-            "newsID" : req.body.newsID,
-            // "commentID" : req.body.commentID,
-            "commenterName" : req.body.commenterName,
-            "content" : req.body.content,
-            "date" : req.body.date,
-            "isResponse" : req.body.isResponse,
-            // Empty if isResponse is false
-            "replyTo" : req.body.replyTo
-        };
-
-        // console.log(stringify(dataToAdd));
-
-        comp590DB.collection("comments").insertOne(dataToAdd, function(err, result) {
-            if (err) throw err;
-            console.log("Inserted comment successfully");
-            // return new comment's _id
-            res.json(result["insertedId"]);
-            db.close();
-        });
-    });
-});
-
-// Current comment's response list.
-app.get('/get_responses', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        console.log("Connected to MongoDB"); 
-
-        var comp590DB = db.db("Comp590");
-        var responseToFind = {
-            "newsID" : req.body.newsID,
-            "isResponse" : req.body.replyTo,
-            // to which comment's id you reply
-            "replyTo" : req.body.replyTo
-        };
-
-        comp590DB.collection("comments").find(responseToFind).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.json(result[0]);
-            db.close();
-        });        
-    });
-});
-
-
-// Run the server
-var server = app.listen(port, function () {
- 
-    var host = server.address().address
-    var port = server.address().port
-   
-    console.log("Running on http://%s:%s", host, port)
+const server = app.listen(port, () => {
+    const addr = server.address();
+    if (addr != null) {
+        const address = addr
+        // // if (addr as AddressInfo)
+        console.log(`Server listening at https://${address.address}:${address.port}`)
+    }
 });
--- src/auth.js
+++ src/auth.js
@@ -1,51 +1,12 @@
 const md5 = require('md5');
 const {v4: uuid} = require('uuid');
 const session = require('express-session');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('./userSchema');
-
-// let sessionUser = {};
 let cookieKey = "sid";
-
-// let userObjs = {};
 // todo: update redis addr
-const redis = require('redis').createClient("redis://:p3384f9050dcc31c341d827fbc269afc9682d98c51740333f7e4df1a930148fb9@ec2-3-220-161-139.compute-1.amazonaws.com:12269");
 
-// passport.serializeUser(function (user, done) {
-//     done(null, user);
-// });
-//
-// passport.deserializeUser(function (user, done) {
-//     done(null, user);
-// });
-//
-// passport.use(new GoogleStrategy({
-//             clientID: '460405852651-rt87fg5moas7f38qhd31d26tba6g83fp.apps.googleusercontent.com',
-//             clientSecret: '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',
-//             callbackURL: "/auth/google/callback"
-//         },
-//         function (accessToken, refreshToken, profile, done) {
-//             let user = {
-//                 /*'email': profile.emails[0].value,
-//                 'name' : profile.name.givenName + ' ' + profile.name.familyName,
-//                 'id'   : profile.id,*/
-//                 'token': accessToken
-//             };
-//             // You can perform any necessary actions with your user at this point,
-//             // e.g. internal verification against a users table,
-//             // creating new user entries, etc.
-//
-//             // return done(null, user);
-//             User.findOrCreate({googleId: profile.id}, function (err, user) {
-//                 if (err) {
-//                     return done(err);
-//                 }
-//                 return done(null, user);
-//             });
-//         })
-// );
-
+const redis = require('redis').createClient(process.env.REDIS_URL ? process.env.REDIS_URL : "");
+const redis_session_key = 'sessions';
 
 function isLoggedIn(req, res, next) {
     // likely didn't install cookie parser
@@ -59,7 +20,7 @@ function isLoggedIn(req, res, next) {
     if (!sid) {
         return res.sendStatus(401);
     }
-    redis.hget('sessions', sid, (err, user) => {
+    redis.hget(redis_session_key, sid, (err, user) => {
         if (err)
             return res.sendStatus(401);
 
@@ -83,13 +44,8 @@ async function login(req, res) {
         return res.sendStatus(400);
     }
 
-    // let user = userObjs[username];
-    //
-    // if (!user) {
-    //     return res.sendStatus(401)
-    // }
     const userObj = await User.findOne({username: username});
-    if (userObj.length === 0) {
+    if (!userObj || userObj.length === 0) {
         return res.sendStatus(404);
     }
 
@@ -99,14 +55,14 @@ async function login(req, res) {
 
     const sid = uuid()
 
-    redis.hmset('sessions', sid, JSON.stringify(userObj));
+    await redis.hmset(redis_session_key, sid, userObj.username);
     res.cookie(cookieKey, sid, {maxAge: 3600 * 1000, httpOnly: true});
     return res.status(200).send({"username": username, "result": "success"});
 }
 
 async function register(req, res) {
     let usercheck = await User.find({username: req.body.username});
-    if (usercheck.length !== 0) {
+    if (!usercheck && usercheck.length !== 0) {
         return res.status(403).send({result: "Username already exists."});
     }
     let username = req.body.username;
@@ -135,19 +91,19 @@ async function register(req, res) {
 
 function logout(req, res) {
     let sid = req.cookies[cookieKey];
-    redis.hdel('sessions', sid);
-    delete req.cookies[cookieKey]
+    redis.hdel(redis_session_key, sid);
+    res.clearCookie(cookieKey);
     res.send({result: "success"})
 }
 
 
 async function password(req, res) {
     let password = req.body.password;
-
+    if (!password || password === "") {
+        return response.sendStatus(400)
+    }
     await User.findOneAndUpdate({username: req.username}, {password: password});
     res.send({result: "success", username: req.username})
-
-
 }
 
 module.exports = (app) => {
@@ -156,21 +112,10 @@ module.exports = (app) => {
         resave: true,
         saveUninitialized: true
     }));
-
-    // app.use(passport.initialize());
-    // app.use(passport.session());
-
     app.post('/login', login);
     app.post('/register', register);
     app.use(isLoggedIn);
     app.put('/logout', logout);
     app.put('/password', password);
 
-    // app.get('/auth/google', passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}));
-    //
-    // app.get('/auth/google/callback',
-    //     passport.authenticate('google', {
-    //         successRedirect: 'http://sh106frontend.surge.sh',
-    //         failureRedirect: 'http://sh106frontend.surge.sh'
-    //     }));
 }
